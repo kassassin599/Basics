a496033eea47ba6bc8bb4b54e21b148c4113b673
Keshav Sharma <kassassin99@gmail.com>
2018-12-01 22:12:33 +0530

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour {
+
+    bool gamehasEnded = false;
+    public float restartDelay = 1f;
+    public GameObject completeLevel1UI;
+
+    public void CompleteLevel1()
+    {
+        completeLevel1UI.SetActive(true); 
+    }
+
+    public void EndGame()
+    {
+        if(gamehasEnded== false)
+        {
+            gamehasEnded = true;
+            Debug.Log("GAME OVER");
+            Invoke("Restart", restartDelay);
+        }
+        
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
